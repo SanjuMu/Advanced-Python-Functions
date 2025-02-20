e90811b89f8ea7311569fed018d72e1038eb3716
SanjuMu <sanjusri.munigala@gmail.com>
2025-02-20 18:12:00 -0500

    project

--- project.py
+++ project.py
@@ -5,6 +5,12 @@ odd_numbers = [x for x in range(n) if x % 2 != 0]
 
 print("Odd Numbers List: ", odd_numbers)
 
+fruits = ['apple', 'banana', 'strawberry']
+
+cap_fruits = [fruit.capitalize() for fruit in fruits]
+
+print("Capitalized Fruits List: ", cap_fruits)
+
 #Note to Teacher: I didn't understand what "another odd numbers" meant
 
 
