3c6d8ba8678f80e60af70b9dbcf2e06d901d67e1
SanjuMu <sanjusri.munigala@gmail.com>
2025-02-20 18:08:49 -0500

    project

---
+++ project.py
@@ -0,0 +1,11 @@
+
+n = int(input("Enter a number: "))
+
+odd_numbers = [x for x in range(n) if x % 2 != 0]
+
+print("Odd Numbers List: ", odd_numbers)
+
+#Note to Teacher: I didn't understand what "another odd numbers" meant
+
+
+
